759ee9b8a91582e32b03cd7dce109a9351de8f45
SOunit <workbookwork7@gmail.com>
2022-01-21 12:59:58 -0800

    add samples

---
+++ 01_youtube/if.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 5
+
+	if x > 4 {
+		fmt.Println("more than 4, ", x)
+	} else if x < 2 {
+		fmt.Println("less than 2")
+	} else {
+
+	}
+}
---
+++ 01_youtube/var.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x int = 5
+	y := 15
+	var sum int = x + y
+	
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(sum)
+}
