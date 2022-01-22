2376db26ac504a310669348302347999e4fb5078
SOUnit <workbookwork7@gmail.com>
2022-01-21 16:09:40 -0800

    add samples

---
+++ 01_youtube/loop.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for loop
+	fmt.Println("------------------ i")
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("------------------ j")
+
+	// while loop
+	j := 0
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
+
+	// array loop
+	fmt.Println("--------------- arr")
+	arr := []int{1, 2, 3, 4, 5}
+	for index, value := range arr {
+		fmt.Print("index", index)
+		fmt.Println("value", value)
+	}
+}
